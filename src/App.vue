f9f0bd32e2a5bda8e193a8822f5d10083ebd7c9b
SimoneDUrso <simone.durso98@gmail.com>
2024-10-14 17:38:59 +0200

    [UPD] Added pagination. Exercise and bonus completed

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,9 @@ export default {
   data(){
     return{
       projects: [],
+
+      first_page: 1, 
+      last_page: null,
     }
   },
 
@@ -22,11 +25,22 @@ export default {
       axios.get('http://127.0.0.1:8000/api/projects').then((response) => {
         // console.log(response.data.results);
 
-        this.projects = response.data.results;
+        this.projects = response.data.results.data;
+        this.current_page = response.data.results.current_page;
+        this.last_page = response.data.results.last_page;
 
+      });
+    },
+
+    paginate(page){
+      this.current_page = page;
+
+      axios.get('http://127.0.0.1:8000/api/projects', {params: { page: page}}).then((response) => {
+        this.projects = response.data.results.data;
       });
     }
   },
+  
   
 }
 </script>
@@ -35,6 +49,22 @@ export default {
   <div class="container">
     <div class="row">
       <ProjectCard v-for="project in projects" :key="project.id" :project="project"/>
+    </div>
+
+    <div class="d-flex justify-content-center">
+      <nav aria-label="...">
+        <ul class="pagination">
+          <li class="page-item">
+            <a class="page-link" :class="current_page == 1 ? 'disabled' : '' " href="#" @click="paginate(current_page - 1)">Precedente</a>
+          </li>
+  
+          <li class="page-item" v-for="i in last_page"><a class="page-link" href="#" @click="paginate(i)"> {{ i }} </a></li>
+  
+          <li class="page-item">
+            <a class="page-link" href="#" :class="current_page == last_page ? 'disabled' : '' " @click="paginate(current_page + 1)">Successiva</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   </div>
 </template>
